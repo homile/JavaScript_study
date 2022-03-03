c273fe4664ded2b9cda10bb3d08c866800665105
homile <ssanho2@naver.com>
2022-03-03 18:45:37 +0900

    10430번_나머지

---
+++ baekjoon/기초문법/입출력과_사칙연산/10430번_나머지.js
@@ -0,0 +1,33 @@
+/* 나머지
+링크: https://www.acmicpc.net/problem/10430
+
+[문제]
+(A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+
+(A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+
+세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+
+[입력]
+첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
+
+[출력]
+첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 
+셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+*/
+
+// 문제풀이(1)
+function solution(input){
+    //const num = require("fs").readFileSync('/dev/stdin').toString().split();
+    const num = input.trim().split(' ');
+    const A = parseInt(num[0]);
+    const B = parseInt(num[1]);
+    const C = parseInt(num[2]);
+
+    console.log((A+B)%C);
+    console.log(((A%C) + (B%C))%C);
+    console.log((A*B)%C);
+    console.log(((A%C) * (B%C))%C);
+}
+
+console.log(solution('5 8 4'));
