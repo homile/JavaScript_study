0dd4e00ab70b0d98fa9cc3aa4b74333d694958b0
homile <ssanho2@naver.com>
2022-03-10 19:40:04 +0900

    11021번 A+B-7

---
+++ baekjoon/기초문법/for문/11021번_A+B-7.js
@@ -0,0 +1,37 @@
+/* A+B-7
+링크: https://www.acmicpc.net/problem/11021
+
+[문제]
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+[입력]
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+[출력]
+각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 
+테스트 케이스 번호는 1부터 시작한다.
+*/
+
+// 문제풀이(1)
+function solution(input){
+   // const num = require("fs").readFileSync('/dev/stdin').toString();
+   const num = input.split('\n');
+
+   for (let i = 1; i <= Number(num[0]); i++){
+      let numbers = num[i].split(' ');
+      let answer = 0;
+      for (let j = 0; j < numbers.length; j++){
+         answer = answer + Number(numbers[j]);
+      }
+      console.log(`Case #${i}: ${answer}`);
+   }
+}
+
+console.log(solution(`5
+1 1
+2 3
+3 4
+9 8
+5 2`));
