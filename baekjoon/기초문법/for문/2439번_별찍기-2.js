55690d4d69e702d6f2aea7b426524bd97376324e
homile <ssanho2@naver.com>
2022-03-10 19:56:13 +0900

    2439번 별 찍기-2

---
+++ baekjoon/기초문법/for문/2439번_별찍기-2.js
@@ -0,0 +1,28 @@
+/* 별 찍기 - 2
+링크: https://www.acmicpc.net/problem/2439
+
+[문제]
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+
+[입력]
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+[출력]
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+*/
+
+// 문제풀이(1)
+function solution(input){
+   // const num = require("fs").readFileSync('/dev/stdin').toString();
+   const num = input;
+
+   for (let i = 1; i <= Number(num); i++){
+      let answer = '';
+      answer = answer + ' '.repeat(Number(num)-i) + '*'.repeat(i);
+      console.log(answer);
+   }
+}
+
+console.log(solution(5));
