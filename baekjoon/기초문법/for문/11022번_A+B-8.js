7ec1f5a1adf50f8fafddb2e1eddc50fac745fcab
homile <ssanho2@naver.com>
2022-03-10 19:43:19 +0900

    11022번 A+B-8

---
+++ baekjoon/기초문법/for문/11022번_A+B-8.js
@@ -0,0 +1,37 @@
+/* A+B - 8
+링크: https://www.acmicpc.net/problem/11022
+
+[문제]
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+[입력]
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+[출력]
+각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. 
+x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
+*/
+
+// 문제풀이(1)
+function solution(input){
+   // const num = require("fs").readFileSync('/dev/stdin').toString();
+   const num = input.split('\n');
+
+   for (let i = 1; i <= Number(num[0]); i++){
+      let numbers = num[i].split(' ');
+      let answer = 0;
+      for (let j = 0; j < numbers.length; j++){
+         answer = answer + Number(numbers[j]);
+      }
+      console.log(`Case #${i}: ${numbers[0]} + ${numbers[1]} = ${answer}`);
+   }
+}
+
+console.log(solution(`5
+1 1
+2 3
+3 4
+9 8
+5 2`));
