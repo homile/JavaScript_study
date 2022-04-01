e74c63ddc89ef32d182c6ca0a7c9678b1c67344c
homile <ssanho2@naver.com>
2022-04-01 12:29:41 +0900

    1157번 단어 공부

---
+++ baekjoon/기초문법/문자열/1157번_단어_공부.js
@@ -0,0 +1,30 @@
+/* 단어 공부
+링크: https://www.acmicpc.net/problem/1157
+*/ 
+
+// 문제풀이(1)
+function solution(input){
+   //const word = require('fs').readFileSync('/dev/stdin').toString().toLowerCase();
+   const word = input.toLowerCase();
+   const alpha = [...new Set(word.split(''))];
+   let max = 0;
+   let max_str = '';
+
+   for (let i = 0; i < alpha.length; i++){
+      // 인덱스 i에 들어있는 문자로 끊어준뒤 length를 알아내면 됨.
+      let cnt = word.split(alpha[i]).length-1;
+
+      if (cnt > max){
+         max = cnt;
+         max_str = alpha[i];
+      }else if(cnt === max){
+         max_str = '?';
+      }
+   }
+   console.log(max_str.toUpperCase());
+
+   // console.log(word, word.split(alpha[2]));
+
+}
+
+console.log(solution(`baaa`));
