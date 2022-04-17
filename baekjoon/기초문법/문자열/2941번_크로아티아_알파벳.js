af0ea4e4fd1265a69d825978b667d7a6874ef37a
homile <ssanho2@naver.com>
2022-04-17 16:46:48 +0900

    2941번 크로아티아 알파벳

---
+++ baekjoon/기초문법/문자열/2941번_크로아티아_알파벳.js
@@ -0,0 +1,14 @@
+/* 크로아티아 알파벳
+링크: https://www.acmicpc.net/problem/2941
+
+[문제접근]
+
+*/ 
+
+// 문제풀이(1)
+function solution(input){
+
+}
+
+console.log(solution(`ljes=njak`));    // 6
+console.log(solution(`nljj`));    // 3
