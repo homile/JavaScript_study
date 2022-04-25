b771a584eb7995bf3d9d3983056f066ef15b9039
homile <ssanho2@naver.com>
2022-04-25 20:06:31 +0900

    10808번 알파벳개수 2번째풀이(1번째 = 문자열)

--- baekjoon/Algorithm/10809번_알파벳찾기.js
+++ baekjoon/Algorithm/10809번_알파벳찾기.js
@@ -4,6 +4,31 @@
 
 // 문제풀이(1)
 function solution(input){
+   //const s = require('fs').readFileSync('/dev/stdin').toString().split('');
+   const s = input.split('');
+   let stack = [];
+   let answer = [];
+   // 대문자: 56 ~ 90, 소문자: 97 ~ 122
+   for (let i = 97; i <= 122; i++){
+      // console.log(String.fromCharCode(i));
+      stack.push(String.fromCharCode(i));
+      answer.push(-1);
+   }
+
+   for (let i = 0; i < s.length; i++){
+      // console.log(stack[s[i]]);
+      for (let j = 0; j < 26; j++){
+         if (s[i] === stack[j]){
+            if (answer[j] !== -1){
+               continue;
+            }
+            answer[j] = i;
+         }
+      }
+   }
+   console.log(answer);
+
+   console.log(answer.join(' '));
 
 }
 
