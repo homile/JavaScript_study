999c09c13edfc638850c26bfc1510b66aaab3cb8
homile <ssanho2@naver.com>
2022-05-04 23:53:42 +0900

    2743번 단어길이재기 풀이

---
+++ baekjoon/Algorithm/2743번_단어길이재기.js
@@ -0,0 +1,13 @@
+/* 단어 길이 재기
+링크: https://www.acmicpc.net/problem/2743
+ */
+
+// 문제풀이(1)
+function solution(input){
+   //const str = require('fs').readFileSync('/dev/stdin').toString().trim();
+   const str = input;
+
+   console.log(str.length);
+}
+
+console.log(solution(`pulljima`));
