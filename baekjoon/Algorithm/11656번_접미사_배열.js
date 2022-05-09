740c93b112b1489fbe4c09e368983563ab8e21ec
homile <ssanho2@naver.com>
2022-05-10 00:14:08 +0900

    11656번_접미사_배열 문제 풀이

--- baekjoon/Algorithm/11656번_접미사_배열.js
+++ baekjoon/Algorithm/11656번_접미사_배열.js
@@ -1,3 +1,17 @@
 /* 접미사 배열
 링크: https://www.acmicpc.net/problem/11656
 */
+
+function solution(input){
+    //const str = require('fs').readFileSync('/dev/stdin').toString().trim();
+   const str = input;
+
+   let stack = [];
+
+   for (let i = 0; i < str.length; i++){
+      stack.push(str.slice(i));
+   }
+   console.log(stack.sort().join('\n'));
+}
+
+console.log(solution(`baekjoon`));
