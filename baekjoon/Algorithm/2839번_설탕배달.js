25ade7bf3015990cab08bfc8176eeec9d7bafac3
homile <ssanho2@naver.com>
2022-05-10 22:23:18 +0900

    2839번 설탕배달 문제

---
+++ baekjoon/Algorithm/2839번_설탕배달.js
@@ -0,0 +1,12 @@
+/* 설탕 배달
+링크: https://www.acmicpc.net/problem/2839
+*/
+
+function solution(input){
+   const num = input;
+
+   
+}
+
+console.log(solution(`18`));  // 4
+console.log(solution(`4`));   // -1
