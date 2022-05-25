438b74ff3a5f31d539b3f7a5ed3d2fd8e08d4ad2
homile <ssanho2@naver.com>
2022-05-25 23:50:48 +0900

    파일 경로 변경

--- baekjoon/Algorithm/math1/10872번 팩토리얼.js
+++ baekjoon/Algorithm/math1/10872번 팩토리얼.js
@@ -4,7 +4,7 @@
 
 // 문제풀이(1)
 function solution(input){
-  //const numbers = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+  //const num = require('fs').readFileSync('/dev/stdin').toString();
   const num = input;
   let total = 1;
   
---
+++ baekjoon/Algorithm/math1/1934번 최소공배수.js
@@ -0,0 +1,17 @@
+/* 최소공배수
+링크: https://www.acmicpc.net/problem/1934
+*/ 
+
+function solution(input){
+   const numbers = input.split('\n');
+}
+
+console.log(solution(`3
+1 45000
+6 10
+13 17`));
+
+// output
+// 45000
+// 30
+// 221
