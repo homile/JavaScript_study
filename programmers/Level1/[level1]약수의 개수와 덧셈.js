242d40c552b6c8f732af40755cc4f63e0f437759
homile <ssanho2@naver.com>
2022-07-20 21:53:31 +0900

    [level1]약수의 개수와 덧셈 문제 등록

---
+++ programmers/Level1/[level1]약수의 개수와 덧셈.js
@@ -0,0 +1,11 @@
+// 약수의 개수와 덧셈
+// 링크: https://school.programmers.co.kr/learn/courses/30/lessons/77884
+
+// 문제풀이(1)
+function solution(left, right) {
+  var answer = 0;
+  return answer;
+}
+
+console.log(solution(13, 17));  // 43
+console.log(solution(24, 27));  // 52
