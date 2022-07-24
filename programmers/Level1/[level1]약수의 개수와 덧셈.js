ae0970d57004631459a237bdbc9b03bdd02afb65
homile <ssanho2@naver.com>
2022-07-25 01:24:26 +0900

    [level1]약수의 개수와 덧셈 문제풀이

--- programmers/Level1/[level1]약수의 개수와 덧셈.js
+++ programmers/Level1/[level1]약수의 개수와 덧셈.js
@@ -3,7 +3,17 @@
 
 // 문제풀이(1)
 function solution(left, right) {
-  var answer = 0;
+  let answer = 0;
+  
+  for(let i = left; i <= right; i++){
+      let count = 0;
+      for(let j = 1; j <= i; j++){
+          if (i % j === 0) count += 1;
+          
+      }
+      count % 2 === 0 ? answer += i : answer -= i;
+  }
+  
   return answer;
 }
 
