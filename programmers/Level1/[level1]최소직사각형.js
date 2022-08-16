62ab86b81b147c0a989698bbc54d0b2cd223e4ba
homile <ssanho2@naver.com>
2022-08-16 23:34:35 +0900

    [level1]최소직사각형 문제풀이

---
+++ programmers/Level1/[level1]최소직사각형.js
@@ -0,0 +1,13 @@
+// 최소직사각형
+// 링크: https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=javascript
+
+// 문제풀이(1)
+function solution(sizes) {
+  let answer = 0;
+
+  return answer;
+}
+
+console.log(solution([[60, 50], [30, 70], [60, 30], [80, 40]]));  // 4000
+console.log(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]));  // 120
+console.log(solution([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]));  // 133
