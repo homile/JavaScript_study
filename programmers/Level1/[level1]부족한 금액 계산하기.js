18e3f583538b6ac7443091a9535aa7a0c41c114d
homile <ssanho2@naver.com>
2022-08-27 23:51:46 +0900

    [level1]부족한 금액 계산하기 문제

---
+++ programmers/Level1/[level1]부족한 금액 계산하기.js
@@ -0,0 +1,11 @@
+// 부족한 금액 계산하기
+// 링크: https://school.programmers.co.kr/learn/courses/30/lessons/82612
+
+// 문제풀이(1)
+function solution(price, money, count) {
+  let answer = -1;
+
+  return answer;
+}
+
+console.log(solution*(3, 20, 4));   // 10
