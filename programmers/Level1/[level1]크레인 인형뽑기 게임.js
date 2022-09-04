8ce90de7e23edee1a11b3dcb2ac6a6f42cdf2d37
homile <ssanho2@naver.com>
2022-09-04 23:15:00 +0900

    [level1]크레인 인형뽑기 게임 문제 작성

---
+++ programmers/Level1/[level1]크레인 인형뽑기 게임.js
@@ -0,0 +1,21 @@
+// 크레인 인형뽑기 게임
+// 링크: https://school.programmers.co.kr/learn/courses/30/lessons/64061
+
+// 문제풀이(1)
+function solution(board, moves) {
+  let answer = 0;
+  return answer;
+}
+
+console.log(
+  solution(
+    [
+      [0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 3],
+      [0, 2, 5, 0, 1],
+      [4, 2, 4, 4, 2],
+      [3, 5, 1, 3, 1],
+    ],
+    [1, 5, 3, 5, 1, 2, 1, 4]
+  )
+);    // 4
