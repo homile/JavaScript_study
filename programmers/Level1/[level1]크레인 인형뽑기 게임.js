64cdd37dd5bbf473386512b35b8847e804deec96
homile <ssanho2@naver.com>
2022-09-05 00:53:02 +0900

    [level1]크레인 인형뽑기 게임 문제 풀이 완료

--- programmers/Level1/[level1]크레인 인형뽑기 게임.js
+++ programmers/Level1/[level1]크레인 인형뽑기 게임.js
@@ -1,10 +1,50 @@
 // 크레인 인형뽑기 게임
 // 링크: https://school.programmers.co.kr/learn/courses/30/lessons/64061
 
-// 문제풀이(1)
+// 문제풀이(1)    // 테스트케이스 1,2번 x
 function solution(board, moves) {
-  let answer = 0;
-  return answer;
+  let count = 0;
+  const stack = [];
+
+  for (let i = 0; i < moves.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (board[j][moves[i] - 1] !== 0) {
+        stack.push(board[j][moves[i] - 1]);
+        board[j][moves[i] - 1] = 0;
+        break;
+      }
+    }
+    if (stack[stack.length - 2] === stack[stack.length - 1]) {
+      stack.pop();
+      stack.pop();
+      count += 2;
+    }
+  }
+
+  return count;
+}
+
+// 문제풀이(2)
+function solution(board, moves) {
+  let count = 0;
+  const stack = [];
+
+  for (let i = 0; i < moves.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (board[j][moves[i] - 1] !== 0) {
+        if(stack[stack.length-1] === board[j][moves[i] - 1]){
+          stack.pop();
+          count +=2;
+        }else{
+          stack.push(board[j][moves[i] - 1]);
+        }
+        board[j][moves[i] - 1] = 0;
+        break;
+      }
+    }
+  }
+
+  return count;
 }
 
 console.log(
@@ -18,4 +58,4 @@ console.log(
     ],
     [1, 5, 3, 5, 1, 2, 1, 4]
   )
-);    // 4
+); // 4
