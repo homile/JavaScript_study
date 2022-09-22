e8e841812e9ef243b60cff5ef317d67f38a1534c
homile <ssanho2@naver.com>
2022-09-22 22:53:31 +0900

    [level1] 성격 유형 검사하기 문제

---
+++ programmers/Level1/[level1]성격 유형 검사하기.js
@@ -0,0 +1,14 @@
+// 성격 유형 검사하기
+// 링크: https://school.programmers.co.kr/learn/courses/30/lessons/118666
+
+// 문제풀이(1)
+function solution(survey, choices) {
+  var answer = "";
+  return answer;
+}
+
+console.log(solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5]));
+// "TCMA"
+
+console.log(solution(["TR", "RT", "TR"], [7, 1, 3]));
+// "RCJA"
