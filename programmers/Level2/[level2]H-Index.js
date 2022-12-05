a545d06063c2557f230b51a0b67f6d200ae9a190
homile <ssanho2@naver.com>
2022-12-05 22:20:53 +0900

    [level2]H-Index 문제 풀이 완료

--- programmers/Level2/[level2]H-Index.js
+++ programmers/Level2/[level2]H-Index.js
@@ -29,7 +29,6 @@ function solution(citations) {
     var answer = 0;
     let cnt = 0;
     citations.sort((a, b) => b-a);
-    console.log(citations);
 
     for (let i = 0; i < citations.length; i++){
         if (citations[i] >= i+1){
