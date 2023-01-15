c9409fe3300a2f213128b32f3e0f01c29039e8be
homile <ssanho2@naver.com>
2023-01-15 23:41:38 +0900

    [level1]음양 더하기 문제 풀이 추가 완료

--- programmers/Level1/[level1]음양 더하기.js
+++ programmers/Level1/[level1]음양 더하기.js
@@ -1,6 +1,12 @@
 // 음양 더하기
 // 링크: https://school.programmers.co.kr/learn/courses/30/lessons/76501
 
+/* 
+  [문제접근]
+  signs의 값이 true이면 양수, fasle이면 음수로 
+  absolutes의 값을 변경한 후 더하면 된다.
+*/
+
 // 문제풀이(1)
 function solution(absolutes, signs) {
   let answer = 0;
@@ -12,6 +18,15 @@ function solution(absolutes, signs) {
     answer += absolutes[i];
   }
 
+  return answer;
+}
+
+// 다른 사람의 풀이(1)
+function solution(absolutes, signs) {
+  let answer = 0;
+  for (let i = 0; i < absolutes.length; i++) {
+    signs[i] ? (answer += absolutes[i]) : (answer -= absolutes[i]);
+  }
   return answer;
 }
 
