20fec91bc9184b9015853eac2106a2e452e08620
homile <ssanho2@naver.com>
2023-02-06 18:00:40 +0900

    2292번 벌집 문제풀이

--- baekjoon/기초문법/기본 수학1/2292번_벌집.js
+++ baekjoon/기초문법/기본 수학1/2292번_벌집.js
@@ -1,9 +1,21 @@
 /* 벌집
 링크: https://www.acmicpc.net/problem/2292
-*/ 
+*/
 
-function solution(input){
+// 문제풀이(1)
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 
+const input = require("fs").readFileSync(filePath).toString().trim();
+
+let sum = 1;
+let count = 1;
+
+while (Number(input) > sum) {
+  sum += 6 * count;
+  count++;
 }
 
-console.log(solution(`13`));  // 3
+console.log(count);
+
+// input
+// 13
