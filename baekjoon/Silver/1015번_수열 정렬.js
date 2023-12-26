bd3902b46b8fd74b458f113930e742d4996a112f
homile <ssanho2@naver.com>
2023-12-26 23:56:05 +0900

    [Baekjoon]1015번 수열 정렬 리팩토링 완료

--- baekjoon/Silver/1015번_수열 정렬.js
+++ baekjoon/Silver/1015번_수열 정렬.js
@@ -4,25 +4,42 @@
 // 문제풀이(1)
 // 주어진 수들을 비내림차순 즉 오름차순으로 만드는 수열을 만들면 되는 문제이다.
 // A배열을 오름차순으로 정렬한 것과 정렬하지 않은 기본의 A배열을 가지고 비교하면된다.
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+// const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+// const N = Number(input[0]);
+// const A = input[1].split(" ").map(Number);
+// const sortedA = input[1]
+//   .split(" ")
+//   .map(Number)
+//   .sort((a, b) => a - b);
+
+// const P = Array(N).fill(-1);
+
+// A.forEach((num, idx) => {
+//   P[idx] = sortedA.findIndex((sortedNum, sortedNumIdx) => {
+//     if (sortedNum === num && !P.includes(sortedNumIdx)) return true;
+//   });
+// });
+
+// console.log(P.join(' ')); 
+
+// 문제풀이(2) - 리팩토링
+// 1. 가속성 향상을 위한 변수명 변경
+// 2. Array.prototype.map 과 Array.prototype.indexOf 를 사용하여 코드를 간결하게 작성
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
-const N = Number(input[0]);
-const A = input[1].split(" ").map(Number);
-const sortedA = input[1]
-  .split(" ")
-  .map(Number)
-  .sort((a, b) => a - b);
+const originalArray = input[1].split(" ").map(Number);
+const sortedArray = [...originalArray].sort((a, b) => a - b);
 
-const P = Array(N).fill(-1);
-
-A.forEach((num, idx) => {
-  P[idx] = sortedA.findIndex((sortedNum, sortedNumIdx) => {
-    if (sortedNum === num && !P.includes(sortedNumIdx)) return true;
-  });
+const positionInSortedArray = originalArray.map((num) => {
+  const position = sortedArray.indexOf(num);
+  sortedArray[position] = null; // 이 위치의 값을 null로 변경하여 더 이상 선택되지 않게 한다.
+  return position;
 });
 
-console.log(P.join(' ')); 
+console.log(positionInSortedArray.join(' '));
 
 // 입력
 // 3
